0653425c9cf5b4a53069ed19a92c1bc1abd4b876
Daffeyd Wilbert <daffeydwilbert@gmail.com>
2024-03-28 09:32:18 +0700

    Creating first blogpost

---
+++ src/components/postCard/postCard.jsx
@@ -0,0 +1,24 @@
+import Image from "next/image"
+import styles from "./postCard.module.css"
+import Link from "next/link"
+import dateFormat from 'dateformat';
+const PostCard = ({post}) => {
+
+  return (
+    <div className={styles.container}>
+      <div className={styles.top}>
+        {post.img && <div className={styles.imgContainer}>
+          <Image src={post.img} alt="" fill className={styles.img}/>
+        </div>}
+        <span className={styles.date}>{dateFormat(post.createdAt, "ddd, mmm d, yy")}</span>
+      </div>
+      <div className={styles.bottom}>
+        <h1 className={styles.title}>{post.title}</h1>
+        <p className={styles.desc}>{post.body}</p>
+        <Link className={styles.link} href={`/blog/${post.slug}`}>READ MORE</Link>
+      </div>
+    </div>
+  )
+}
+
+export default PostCard
